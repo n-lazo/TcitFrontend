1a15a3c1eb61495620ccf239a2edf1eecee81ef1
nlazo <nlazo@inputsoft.cl>
2024-10-30 05:01:08 -0300

    Filtro terminado post mediante uso de selector

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,25 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { Post } from '../post.model';
 import { selectPosts } from '../state/post.selectors';
-import {PostActions, PostApiActions} from '../state/post.actions';
-
-
+import { PostActions } from '../state/post.actions';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { startWith, switchMap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-post-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.css'
 })
 export class PostListComponent implements OnInit {
   posts$: Observable<Post[]>;
+  filterForm: FormGroup;
 
-  constructor(private store: Store) {
-    this.posts$ = store.select(selectPosts);
+  constructor(private store: Store, private fb: FormBuilder) {
+    this.filterForm = this.fb.group({
+      nombre: ['']
+    });
+    this.posts$ = this.filterForm.valueChanges.pipe(
+      startWith({ nombre: '' }),
+      switchMap(filter =>
+        this.store.select(selectPosts).pipe(
+          map(posts => posts.filter(post => post.nombre.includes(filter.nombre)))
+        )
+      )
+    );
   }
 
   ngOnInit() {
@@ -29,5 +40,4 @@ export class PostListComponent implements OnInit {
   removePost(id: number) {
     this.store.dispatch(PostActions.remove({ id }));
   }
-
 }
